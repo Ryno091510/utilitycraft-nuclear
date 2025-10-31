7d6898735dcf1e5b0da46d5721811f2a7d89fee1
Ryno091510 <ryan.g.mejeur@gmail.com>
2025-10-31 16:38:13 -0400

    Added uranium loot table, lowered ore breaking time, made the incinerator and blast furnace work for lead, and lowered the needed amount of lead ingots for a plate.

---
+++ BP/scripts/incinerator_additions.js
@@ -0,0 +1,11 @@
+import { system, world } from "@minecraft/server";
+
+world.afterEvents.worldLoad.subscribe(() => {
+    const newRecipes = {
+        "utilitycraft:raw_lead": { output: "utilitycraft:lead_ingot" }
+    };
+
+    system.sendScriptEvent("utilitycraft:register_furnace_recipe", JSON.stringify(newRecipes));
+
+    console.warn("[Addon] Custom furnace recipes registered via system event.");
+});
--- builds/dist/UtilityCraft Nuclear BP/scripts/press_additions.js
+++ builds/dist/UtilityCraft Nuclear BP/scripts/press_additions.js
@@ -2,7 +2,7 @@ import { system, world } from "@minecraft/server";
 
 world.afterEvents.worldLoad.subscribe(() => {
     const newRecipes = {
-        "utilitycraft:lead_ingot": { output: "utlitycraft:lead_plate", required: 4 },
+        "utilitycraft:lead_ingot": { output: "utlitycraft:lead_plate", required: 1 },
     };
 
     system.sendScriptEvent("utilitycraft:register_press_recipe", JSON.stringify(newRecipes));
