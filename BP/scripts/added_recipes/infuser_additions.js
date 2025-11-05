371925ba0fa46c79f9fa9212009728876dd43a6a
Ryno091510 <ryan.g.mejeur@gmail.com>
2025-11-05 14:36:29 -0500

    Added a recipe for the nuclear fuel that uses a nonexistent item, doubled the reactor speed, and halved the power in each unit of fuel.

--- BP/scripts/added_recipes/infuser_additions.js
+++ BP/scripts/added_recipes/infuser_additions.js
@@ -3,7 +3,8 @@ import { system, world } from "@minecraft/server";
 world.afterEvents.worldLoad.subscribe(() => {
     const newRecipes = {
         "utilitycraft:ryno_lead_plate|minecraft:concrete": { output: "utilitycraft:ryno_reactor_concrete", required: 4 },
-        "utilitycraft:ryno_lead_plate|utilitycraft:compressed_glass": { output: "utilitycraft:ryno_reactor_glass", required: 4 }
+        "utilitycraft:ryno_lead_plate|utilitycraft:compressed_glass": { output: "utilitycraft:ryno_reactor_glass", required: 4 },
+        "utilitycraft:ryno_refined_uranium|utilitycraft:ryno_empty_fuel_cell": { output: "utilitycraft:ryno_nuclear_fuel", required: 8 }
     };
 
     system.sendScriptEvent("utilitycraft:register_infuser_recipe", JSON.stringify(newRecipes));
--- BP/scripts/config/recipes/basic_reactor.js
+++ BP/scripts/config/recipes/basic_reactor.js
@@ -1,4 +1,4 @@
 export const nuclearFuels = [
-    { id: "utilitycraft:ryno_refined_uranium", de: 400000 },
-    { id: "utilitycraft:ryno_nuclear_fuel", de: 4000000 }
+    { id: "utilitycraft:ryno_refined_uranium", de: 200000 },
+    { id: "utilitycraft:ryno_nuclear_fuel", de: 2000000 }
 ];
