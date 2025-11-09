c1250eb746e18fbe1785b739a2ccbf8944ce8db4
Ryno091510 <ryan.g.mejeur@gmail.com>
2025-11-09 15:27:09 -0500

    Made the basic reactor craftable

--- BP/scripts/added_recipes/assembler_additions.js
+++ BP/scripts/added_recipes/assembler_additions.js
@@ -7,6 +7,10 @@ world.afterEvents.worldLoad.subscribe(() => {
         "ryno_lead_plate,ryno_lead_ingot,ryno_lead_plate,ryno_lead_plate,utiltycraft:ryno_reactor_glass,ryno_lead_plate,ryno_lead_plate,ryno_lead_ingot,ryno_lead_plate": {
             output: "utilitycraft:ryno_empty_fuel_cell",
             amount: 1
+        },
+        "ultimate_chip,ryno_reactor_glass,ultimate_chip,ryno_reactor_concrete,lead_ingot,ryno_reactor_concrete,ryno_reactor_concrete,ultimate_chip,ryno_reactor_concrete": {
+            output: "utilitycraft:ryno_basic_reactor",
+            amount: 1
         }
     };
     system.sendScriptEvent("utilitycraft:register_crafter_recipe", JSON.stringify(newRecipes));
